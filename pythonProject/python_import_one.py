3ff9758ea665a82494a5d174ef4f2e2aad706e77
user <chetanparate1989@gmail.com>
2021-09-07 11:08:15 +0530

    added new python files

---
+++ pythonProject/python_import_one.py
@@ -0,0 +1,12 @@
+
+def printchet(string):
+    return f"Ye string chetan ko de de thakur{string}"
+
+def add(numone, numtwo):
+    return numone+numtwo+6
+
+print("and the name is",__name__)
+if __name__ == '__main__':
+    print(printchet(" Chetan"))
+    out = add(4,6)
+    print(out)
---
+++ pythonProject/python_import_two.py
@@ -0,0 +1,3 @@
+import python_import_one as adding
+
+print(adding.add(5,8))
